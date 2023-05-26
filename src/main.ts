1fe8150c9c695d2f031be3475c91348183d4d3e0
Phuc <phucnguyen230496@gmail.com>
2023-05-26 23:59:35 +0700

    feat#3: add environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { importProvidersFrom } from '@angular/core';
+import { enableProdMode, } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import {  bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app/app-routing';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(ROUTES)],
 }).catch((err) => console.error(err));
+
+if (environment.production) {
+  enableProdMode();
+}
